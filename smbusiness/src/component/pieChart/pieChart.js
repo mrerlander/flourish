fd1d850fb1a38a7807f0138dd71fbdf8f16bf97c
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-04-12 16:17:24 -0500

    chart refinements

--- smbusiness/src/component/pieChart/pieChart.js
+++ smbusiness/src/component/pieChart/pieChart.js
@@ -5,17 +5,18 @@ import {Row, Col} from 'react-materialize';
 const PieChart = props => (
     <Row>
         <Col 
-            s={6}
-            offset='s3'
+            s={4}
+            offset='s4'
         >
             <VictoryPie
-            padding={50}
-            colorScale={["tomato", "navy", "green" ]} 
-            data={[
-                { x: (props.fixed > 0) ? "Fixed" : "", y: ((props.fixed / props.revenue) * 100) },
-                { x: (props.variable > 0) ? "Variable" : "", y: ((props.variable / props.revenue) * 100) },
-                { x: (props.revenue > 0) ? "Profit" : "", y: (((props.revenue - props.fixed - props.variable) / props.revenue) * 100) }
-              ]}/>
+                padding={{top: 0, right: 100, bottom: 20, left: 100}}
+                colorScale={["tomato", "navy", "green" ]} 
+                data={[
+                    { x: (props.fixed > 0) ? "Fixed" : null, y: ((props.fixed / props.revenue) * 100)},
+                    { x: (props.variable > 0) ? "Variable" : null, y: ((props.variable / props.revenue) * 100)},
+                    { x: (props.revenue > 0) ? "Profit" : null, y: (((props.revenue - props.fixed - props.variable) / props.revenue) * 100)}
+                ]}
+            />
         </Col>
     </Row>
 )
