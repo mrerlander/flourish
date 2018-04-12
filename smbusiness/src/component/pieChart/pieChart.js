d80db946f7994f93099f1efefc2321970e3f24ef
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-04-12 15:29:20 -0500

    added initial pie chart functionality

--- smbusiness/src/component/pieChart/pieChart.js
+++ smbusiness/src/component/pieChart/pieChart.js
@@ -5,10 +5,17 @@ import {Row, Col} from 'react-materialize';
 const PieChart = props => (
     <Row>
         <Col 
-            s={4}
-            offset='s4'
+            s={6}
+            offset='s3'
         >
-            <VictoryPie />
+            <VictoryPie
+            padding={50}
+            colorScale={["tomato", "navy", "green" ]} 
+            data={[
+                { x: (props.fixed > 0) ? "Fixed" : "", y: ((props.fixed / props.revenue) * 100) },
+                { x: (props.variable > 0) ? "Variable" : "", y: ((props.variable / props.revenue) * 100) },
+                { x: (props.revenue > 0) ? "Profit" : "", y: (((props.revenue - props.fixed - props.variable) / props.revenue) * 100) }
+              ]}/>
         </Col>
     </Row>
 )
--- smbusiness/src/pages/demo.js
+++ smbusiness/src/pages/demo.js
@@ -13,16 +13,31 @@ class Demo extends Component {
             fixed: 0,
             variable: 0,
             revenue: 0,
-            goal: 0
+            goal: 0,
         }
     };
+
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+
+        this.setState({
+          [name]: value
+        });
+    }
 
     render(){
         return (
             <div>
                 <Header />
-                <Form />
-                <PieChart />
+                <Form 
+                    handleInputChange={this.handleInputChange}
+                />
+                <PieChart 
+                    fixed={this.state.fixed}
+                    variable={this.state.variable}
+                    revenue={this.state.revenue}
+                />
                 <Footer />
             </div>
         )
