26d3242bd2d4d372459b285d8ffcd2d0142f6b15
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-05-08 12:26:01 -0500

    nav link fix

--- flourish/src/component/header/header.js
+++ flourish/src/component/header/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Navbar, NavItem} from 'react-materialize';
+import {NavLink} from 'react-router-dom';
 import './header.css';
 
 
 const Header = () => (
     <Navbar className='teal darken-4' brand='Flourish' right>
-        <NavItem href='/about'>About</NavItem>
-        <NavItem href='/howitworks'>How it Works</NavItem>
-        <NavItem href='contactus'>Contact Us</NavItem>
+        <li><NavLink to='/about'>About</NavLink></li>
+        <li><NavLink to='/howitworks'>How it Works</NavLink></li>
+        <li><NavLink to='contactus'>Contact Us</NavLink></li>
     </Navbar>
 );
 
