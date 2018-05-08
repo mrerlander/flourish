cbd6c1976178069332987b974973c37c9e48ad37
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-05-08 11:04:29 -0500

    rebuilt production version

--- flourish/src/pages/about.js
+++ flourish/src/pages/about.js
@@ -23,7 +23,7 @@ class About extends Component {
           event.preventDefault();
 
         const userEmail = event.target.firstElementChild.firstElementChild.value;
-        debugger;
+        
         fetch("/savedemails", {
           method: 'POST',
           headers: {
