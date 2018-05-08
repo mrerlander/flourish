95b274b901c6076dba1448757ee87dd16518487e
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-05-08 12:27:59 -0500

    bug fix?

--- flourish/src/component/header/header.js
+++ flourish/src/component/header/header.js
@@ -8,7 +8,7 @@ const Header = () => (
     <Navbar className='teal darken-4' brand='Flourish' right>
         <li><NavLink to='/about'>About</NavLink></li>
         <li><NavLink to='/howitworks'>How it Works</NavLink></li>
-        <li><NavLink to='contactus'>Contact Us</NavLink></li>
+        <li><NavLink to='/contactus'>Contact Us</NavLink></li>
     </Navbar>
 );
 
