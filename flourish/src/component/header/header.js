1096e79621c32a9ef4764dcfc4d961367f1269e5
Michael@MIKE-LAPTOP <mrerlander@gmail.com>
2018-05-08 12:32:43 -0500

    bug fix!

--- flourish/src/component/header/header.js
+++ flourish/src/component/header/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, NavItem} from 'react-materialize';
+import {Navbar} from 'react-materialize';
 import {NavLink} from 'react-router-dom';
 import './header.css';
 
